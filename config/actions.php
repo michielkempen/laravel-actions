e6c8347082de3603948af52399ae37104a088948
Michiel Kempen <kempenmichiel@gmail.com>
2019-12-04 23:42:21 +0100

    add documentation

--- config/actions.php
+++ config/actions.php
@@ -2,10 +2,19 @@
 
 return [
 
+    /**
+     * The name of the queue the queued actions should be sent to.
+     */
     'default_queue' => 'default',
 
+    /**
+     * The number of seconds a queued action can run before timing out.
+     */
     'default_timeout' => 60,
 
+    /**
+     * The number of times a queued action should be attempted before failure.
+     */
     'default_attempts' => 1,
 
 ];
